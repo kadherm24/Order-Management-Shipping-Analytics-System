cf077af7c8859010bd928438220fcefd8b452768
kadherm24 <kadherm24@gmail.com>
2026-08-25 20:33:01 +0530

    Product Model

--- order_management/models.py
+++ order_management/models.py
@@ -7,3 +7,12 @@ class Customer(models.Model):
 
     def __str__(self):
         return self.name
+
+class Product(models.Model):
+    product_name = models.CharField(max_length= 100)
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    stock = models.IntegerField()
+    created_at = models.DateField(auto_now_add= True)
+
+    def __str__(self):
+        return self.product_name
